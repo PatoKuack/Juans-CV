5e9a3733c568460c1cf6569ff57f3d84a74ed031
Pato Kuack <nocturno_x_13514@hotmail.com>
2023-03-10 23:10:52 -0600

    9°-3 Se corrigió un posicionamiento de la foto de perfil y se hizo la accesibilidad en movil, por si las moscas =P.

--- src/utils/showMenu.js
+++ src/utils/showMenu.js
@@ -1,16 +1,73 @@
 window.addEventListener('load', () => {
   const toggleId = document.getElementById('main-menu-toggle');
   const navId = document.getElementById('main-nav');
+  const navLink = Array.from(document.querySelectorAll('.main-menu__link'));
   const brandID = document.getElementById('main-brand');
   const mainId = document.getElementById('main-content');
   const footerId = document.getElementById('main-footer');
   
+  const mediaBp = matchMedia('(min-width: 768px)');
+  //*retorna un objeto del tipo MediaQueryList.*
+  /* console.log(mediaBp.matches); */
+  //matches es una propiedad que retorna un booleano.
+
+  //evento que se activa al cargar el documento:
+  // document.addEventListener('DOMContentLoaded', () => {
+    //si la página tiene minimo un ancho de 768px, entonce...
+    if(mediaBp.matches && navLink){
+      //remover los tabindex de los links de navegación.
+      console.log(navLink);
+      navLink.map(e => {
+        e.removeAttribute("tabindex");
+      });
+    }
+  // });
+  window.addEventListener('resize', () => {
+    //si la página tiene minimo un ancho de 768px, entonce...
+    if(mediaBp.matches && navLink) {
+      //remover los tabindex de los links de navegación.
+      navLink.map(e => {
+        e.removeAttribute("tabindex");
+      });
+    } else {
+      navLink.map(e => {
+        e.setAttribute("tabindex", "-1");
+      });
+    }
+  });
+
   if( toggleId && navId && brandID && mainId && footerId ) {
     // Si se da clic en el botón hamburguesa se le colocará al menú la clase 'show'.
     toggleId.addEventListener('click', () => {
-      navId.classList.toggle('show');
-      toggleId.classList.toggle('active');
+      const objClases = navId.classList;
+      const arrayClasses = Object.values(objClases);
+      let confirmation = arrayClasses.includes("show");
+      if(!confirmation) {
+        navId.classList.add('show');
+        toggleId.classList.add('active');
+        //remover los tabindex de los links de navegación.
+        navLink.map(e => {
+          e.removeAttribute("tabindex");
+        });
+      } else {
+        navId.classList.remove('show');
+        toggleId.classList.remove('active');
+        navLink.map(e => {
+          e.setAttribute("tabindex", "-1");
+        });
+      }
     });
+
+    navLink.map(e => {
+      e.addEventListener('click', () => {
+        navId.classList.remove('show');
+        toggleId.classList.remove('active');
+        navLink.map(e => {
+          e.setAttribute("tabindex", "-1");
+        });
+      });
+    });
+
     // Si se da clic fuera del menú, se eliminará la clase 'show'.
     brandID.addEventListener('touchstart', () => {
       removeShowActive(navId, toggleId);
@@ -26,15 +83,16 @@ window.addEventListener('load', () => {
 
   
 const removeShowActive = (navigator, toggle) => {
-  let classNumber = navigator.classList.length;
-  if(classNumber==2){
-    let className = navigator.classList[1];
-    if(className=="show"){
-      navigator.classList.remove('show');
-      toggle.classList.remove('active');
-    }
+  const objClases = navigator.classList;
+  const arrayClasses = Object.values(objClases);
+  let confirmation = arrayClasses.includes("show");
+  if(confirmation){
+    navigator.classList.remove('show');
+    toggle.classList.remove('active');
+    navLink.map(e => {
+      e.setAttribute("tabindex", "-1");
+    });
   }
-
 }
 
 
